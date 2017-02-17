1fab9566d622362d00e5ac12f936df0e5bbfe08e
colinpade <apple@Apples-MacBook-Pro.local>
2017-02-17 14:58:49 -0800

    more user reg

--- src/blueblog/urls.py
+++ src/blueblog/urls.py
@@ -3,9 +3,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from accounts.views import UserRegistrationView
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration'),
+    url(r'^login/$', login, {'template_name' 'login.html'}, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/login/'}, name='logout'),
 ]
